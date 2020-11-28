11d3e49964ba2d3dea6dd8930b45187b35da6ffc
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-11-28 03:46:34 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    roster = {}
+  end
+  def add_student(student, grade)
+    @student = student
+    @grade = grade
+    roster[@grade] = []
+    roster[@grade] << @student
+  end
+  def grade(grade)
+    @grade = grade
+    roster[@grade]
+  end
+  def sort
+    roster.sort
+  end
+end
